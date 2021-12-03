f87b40afc2b5344e15f5544a18d38288da170965
Xun Ruan <galaxymacos@gmail.com>
2021-12-03 06:47:40 -0500

    Initial Commit

---
+++ SwiftAlgorithm/SwiftAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAlgorithmApp.swift
+//  SwiftAlgorithm
+//
+//  Created by xunruan on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
